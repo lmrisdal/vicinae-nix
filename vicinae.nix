c15c7141895810812f3eabb0cddd73b2b5b952c4
Lars Risdal <larsrisdal@gmail.com>
2025-08-29 08:19:50 +0200

    update upstream to v0.8.0

--- vicinae.nix
+++ vicinae.nix
@@ -29,8 +29,8 @@ let
   src = fetchFromGitHub {
     owner = "vicinaehq";
     repo = "vicinae";
-    rev = "v0.7.3";
-    hash = "sha256-ByxpEs9y7nv0wE7DD7zKIScQZMFMeGD/y/EsPxPVZxI=";
+    rev = "v0.8.0";
+    hash = "sha256-VYqpOI+aFOCbtU3ZBGRd8/YEr8AgO6Ruq/H3J9GrosE=";
   };
 
   # Prepare node_modules for api folder
@@ -49,93 +49,95 @@ let
   };
 
 in
-  stdenv.mkDerivation rec {
-    pname = "vicinae";
-    version = src.rev;
+stdenv.mkDerivation rec {
+  pname = "vicinae";
+  version = src.rev;
 
-    inherit src;
+  inherit src;
 
-    cmakeFlags = [
-        "-DCMAKE_INSTALL_PREFIX=${placeholder "out"}"
-        "-DCMAKE_INSTALL_DATAROOTDIR=share"
-        "-DCMAKE_INSTALL_BINDIR=bin"
-        "-DCMAKE_INSTALL_LIBDIR=lib"
-    ];
+  cmakeFlags = [
+    "-DCMAKE_INSTALL_PREFIX=${placeholder "out"}"
+    "-DCMAKE_INSTALL_DATAROOTDIR=share"
+    "-DCMAKE_INSTALL_BINDIR=bin"
+    "-DCMAKE_INSTALL_LIBDIR=lib"
+  ];
 
-    nativeBuildInputs = [
-      ts-protoc-gen-wrapper
-      extensionManagerDeps
-      autoPatchelfHook
-      cmake
-      ninja
-      nodejs
-      pkg-config
-      qt6.wrapQtAppsHook
-      rapidfuzz-cpp
-      protoc-gen-js
-      protobuf
-      grpc-tools
-      which
-      rsync
-      breakpointHook
-      typescript
-    ];
+  nativeBuildInputs = [
+    ts-protoc-gen-wrapper
+    extensionManagerDeps
+    autoPatchelfHook
+    cmake
+    ninja
+    nodejs
+    pkg-config
+    qt6.wrapQtAppsHook
+    rapidfuzz-cpp
+    protoc-gen-js
+    protobuf
+    grpc-tools
+    which
+    rsync
+    breakpointHook
+    typescript
+  ];
 
-    buildInputs = [
-      qt6.qtbase
-      qt6.qtsvg
-      qt6.qttools
-      qt6.qtwayland
-      qt6.qtdeclarative
-      qt6.qt5compat
-      wayland
-      kdePackages.qtkeychain
-      kdePackages.layer-shell-qt
-      minizip
-      grpc-tools
-      protobuf
-      nodejs
-      minizip-ng
-      cmark-gfm
-      libqalculate
-    ];
+  buildInputs = [
+    qt6.qtbase
+    qt6.qtsvg
+    qt6.qttools
+    qt6.qtwayland
+    qt6.qtdeclarative
+    qt6.qt5compat
+    wayland
+    kdePackages.qtkeychain
+    kdePackages.layer-shell-qt
+    minizip
+    grpc-tools
+    protobuf
+    nodejs
+    minizip-ng
+    cmark-gfm
+    libqalculate
+  ];
 
-    configurePhase = ''
-      cmake -G Ninja -B build $cmakeFlags
-    '';
+  configurePhase = ''
+    cmake -G Ninja -B build $cmakeFlags
+  '';
 
-    buildPhase = ''
-      export npm_config_cache=${apiDeps}
-      cd /build/source/api
-      npm i --ignore-scripts
-      patchShebangs /build/source/api
-      npm rebuild --foreground-scripts
-      export npm_config_cache=${extensionManagerDeps}
-      cd /build/source/extension-manager
-      npm i --ignore-scripts
-      patchShebangs /build/source/extension-manager
-      npm rebuild --foreground-scripts
-      cd /build/source
-      substituteInPlace cmake/ExtensionApi.cmake cmake/ExtensionManager.cmake --replace "COMMAND npm install" ""
-      cmake --build build
-      cd /build/source
-    '';
+  buildPhase = ''
+    export npm_config_cache=${apiDeps}
+    cd /build/source/api
+    npm i --ignore-scripts
+    patchShebangs /build/source/api
+    npm rebuild --foreground-scripts
+    export npm_config_cache=${extensionManagerDeps}
+    cd /build/source/extension-manager
+    npm i --ignore-scripts
+    patchShebangs /build/source/extension-manager
+    npm rebuild --foreground-scripts
+    cd /build/source
+    substituteInPlace cmake/ExtensionApi.cmake cmake/ExtensionManager.cmake --replace "COMMAND npm install" ""
+    cmake --build build
+    cd /build/source
+  '';
 
-    dontWrapQtApps = true;
-    preFixup = ''
-        wrapQtApp "$out/bin/vicinae" --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath buildInputs}
-    '';
-    postFixup = ''
-        wrapProgram $out/bin/vicinae \
-        --prefix PATH : ${lib.makeBinPath [
-          nodejs
-          qt6.qtwayland
-          wayland
-          (placeholder "out")
-        ]}
-    '';
+  dontWrapQtApps = true;
+  preFixup = ''
+    wrapQtApp "$out/bin/vicinae" --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath buildInputs}
+  '';
+  postFixup = ''
+    wrapProgram $out/bin/vicinae \
+    --prefix PATH : ${
+      lib.makeBinPath [
+        nodejs
+        qt6.qtwayland
+        wayland
+        (placeholder "out")
+      ]
+    }
+  '';
 
-    installPhase = ''
-      cmake --install build
-    '';
+  installPhase = ''
+    cmake --install build
+  '';
 }
